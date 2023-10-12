a323cbcb14db9f611f3088ba101be1a550d8bbbf
carlangas2503 <carlos.valencias2503@gmail.com>
2023-10-12 16:35:03 -0500

    fin practicas

---
+++ my-store/src/app/reacpt.ts
@@ -0,0 +1,27 @@
+const username: string = 'carlangas2503';
+
+function suma(a:number,b:number) {
+  return a + b
+}
+
+suma(11,10)
+let lastName = 'Valencia'
+let age = 20
+
+// class Person{
+//   age:number;
+//   lastName:string;
+
+//   constructor(age:number,lastName:string){
+//     this.age = age;
+//     this.lastName = lastName;
+//   }
+// }
+//cuando se pone de esta manera la sintaxis queda muy larga y hay formas de hacer lo mismo, solo podiendo los atributos
+// que le llegan al constructor como publicos
+
+class Person{
+  constructor(public age:number, public lastName:string){}
+}
+
+const persona = new Person(age,lastName)
