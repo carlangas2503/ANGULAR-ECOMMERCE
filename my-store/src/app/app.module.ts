d4d9cf051d5a32aebb2fce02175edb6eef1f5dc1
carlangas2503 <carlos.valencias2503@gmail.com>
2023-11-02 16:32:07 -0500

    start routing

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -15,6 +15,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { FavsComponent } from './components/favs/favs.component';
 import { CardsFavComponent } from './components/cards-fav/cards-fav.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +35,13 @@ import { CardsFavComponent } from './components/cards-fav/cards-fav.component';
     ReversePipe,
     FavsComponent,
     CardsFavComponent,
+    NotFoundComponent,
+    CategoryComponent,
+    MycartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
   ],
   imports: [
     SwiperModule,
